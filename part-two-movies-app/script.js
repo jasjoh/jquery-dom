dea8d4513befb3c1b564a2c393aa92e22bad5214
Kirra Hyde <kirra.hyde@gmail.com>
2023-07-10 20:36:31 -0400

    delete button

--- part-two-movies-app/script.js
+++ part-two-movies-app/script.js
@@ -4,11 +4,22 @@ $("#submit").on("click", addMovie);
 function addMovie(evt) {
   evt.preventDefault();
 
-  const movieTitle = $("#title");
-  const movieRating = $("#rating");
+  const $movieTitle = $("#title");
+  const $movieRating = $("#rating");
 
-  const newTitleElement = $("<div>").text(`Movie Title is: ${movieTitle.val()}
-  and Movie Rating is ${movieRating.val()}.`);
+  const $newTitleElement = $("<div>").text(`Movie Title is: ${$movieTitle.val()}
+  and Movie Rating is ${$movieRating.val()}.`);
 
-  $("#Movielist").append(newTitleElement);
+  $("#Movielist").append($newTitleElement);
+
+  const $deleteButton = $("<button>");
+  $deleteButton.on("click", removeMovie);
+  $deleteButton.text("Delete Me");
+  $("#Movielist").append($deleteButton);
+
+}
+
+function removeMovie(evt) {
+  $(evt.target).prev().remove();
+  $(evt.target).remove();
 }
